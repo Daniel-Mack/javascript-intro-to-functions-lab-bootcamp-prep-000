22459a6a47e7c5f164a743daae37a87cd032d2af
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-10 21:59:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,13 @@ function logWhisper(string) {
 logWhisper
 
 function sayHiToGrandma(string) {
-  if str.
+  if (str.toUpperCase() ===  str) {
+    return "YES INDEED!"
+    }
+    else if (str.toLowerCase() === str) {
+      return "I can't hear you!"
+    }
+    else if (str === "I love you, Grandma.") {
+      return "I love you, too."
+    }
 }
